de00ddd55c6c166a896f4271c0c45c18d8db39e2
nehanlkt <nehanlkt@gmail.com>
2026-02-26 15:04:27 +0530

    js

---
+++ matnnew.js
@@ -0,0 +1,8 @@
+// number n math 
+// order of opr :mul,div done first n then plus n negative next same priority from left to right evaluation
+// but if bracket is used then operators dosent matters whatever in bracket that vl evaluates first
+
+// for percent calculation not llike this 36.93*10% but like this 36.93*0.1
+
+// Math.round(2.2)=2  n 2.8=3
+// in dollars like 29.56 this addn vl give inaccurate ans therefore first covert into cent then round fn use
